c780f738470d0062879cc6e2f0757f227474fb09
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-02-27 14:39:16 +0100

    putting import java.until.Scanner

--- src/com/numerosprimitivos/TiposPrimitivos.java
+++ src/com/numerosprimitivos/TiposPrimitivos.java
@@ -1,10 +1,13 @@
 package com.numerosprimitivos;
 
+import java.util.Scanner;
+
 public class TiposPrimitivos {
 
     public static void main(String[] args) {
-        String nome = "Victor";
-        float nota = 9.5f;
+        Scanner escreva = new Scanner(System.in);
+        String nome = escreva.nextLine();
+        float nota = escreva.nextFloat();
         System.out.format("A nota de %s es %.1f \n", nome, nota);
     }
 }
