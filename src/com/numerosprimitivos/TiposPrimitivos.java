7dbb2326602fae00357c2b7b0098c5ec5261797e
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-02-27 14:28:26 +0100

    Initial commit

---
+++ src/com/numerosprimitivos/TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package com.numerosprimitivos;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        String nome = "Victor";
+        float nota = 9.5f;
+        System.out.format("A nota de %s es %.1f \n", nome, nota);
+    }
+}
