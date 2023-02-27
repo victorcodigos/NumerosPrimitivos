20dccde53c139892025710bae3d19890726fd468
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-02-27 14:44:53 +0100

    write your details and follow up

--- src/com/numerosprimitivos/TiposPrimitivos.java
+++ src/com/numerosprimitivos/TiposPrimitivos.java
@@ -6,7 +6,9 @@ public class TiposPrimitivos {
 
     public static void main(String[] args) {
         Scanner escreva = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
         String nome = escreva.nextLine();
+        System.out.print("Digite a nota do aluno: ");
         float nota = escreva.nextFloat();
         System.out.format("A nota de %s es %.1f \n", nome, nota);
     }
